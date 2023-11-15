05e501335904356f0719c8f7aadb6deb465b5abc
shalinbhalala01 <sbhalala6784@students.mc3.edu>
2023-11-14 22:07:07 -0500

    Create Tester.java
    For loop to print 0 - 99. Test GitHub functionaliy

---
+++ Tester.java
@@ -0,0 +1,18 @@
+
+public class Tester {
+
+   public static void main(String[] args) {
+      
+      int counter = 0;
+      
+      for(int i = 0; i < 100; i++) {
+      
+         System.out.println(counter);
+         counter++;
+         
+         
+      }
+   
+   }
+
+}
